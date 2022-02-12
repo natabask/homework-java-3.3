7240fde01d8f8c8c3f20e483202a5b435aa28489
natabask <natabask@yandex.ru>
2022-02-12 11:45:20 +0300

    explanation of the formula added

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,7 +1,12 @@
 public class CreditPaymentService {
     public int calculate(int loanAmount, int loanPeriod) {
-        double annuityRatio = 0.008325 * Math.pow(1.008325, loanPeriod) / (Math.pow(1.008325, loanPeriod) - 1);
+        // процентная ставка (годовая) по условиям задачи = 9,99%
+        double annualInterestRate = 0.0999;
+        // вычисляем процент аннуитета
+        double annuityRatio = annualInterestRate / 12 * Math.pow((1 + annualInterestRate / 12), loanPeriod) / (Math.pow((1 + annualInterestRate / 12), loanPeriod) - 1);
+        // вычисляем размер ежемесячной выплаты (точное значение)
         double exactMonthlyPayment = loanAmount * annuityRatio;
+        // округляем размер ежемесячной выплаты (до рублей)
         int monthlyPayment = (int) exactMonthlyPayment;
         return monthlyPayment;
     }
--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,13 @@ public class Main {
         CreditPaymentService service = new CreditPaymentService();
         // Переменные: s - loan amount [rubles]
         int s = 1_000_000;
-        // Loan for 1 year
+        // Loan for 1 year (12 month)
         int monthlyPayment1 = service.calculate(s, 12);
         System.out.println("If you take 1 million rubles for 1 year, you have to pay the bank " + monthlyPayment1 + " rubles a month");
-        // Loan for 2 years
+        // Loan for 2 years (24 month)
         int monthlyPayment2 = service.calculate(s, 24);
         System.out.println("If you take 1 million rubles for 2 years, you have to pay the bank " + monthlyPayment2 + " rubles a month");
-        // Loan for 3 years
+        // Loan for 3 years (36 month)
         int monthlyPayment3 = service.calculate(s, 36);
         System.out.println("If you take 1 million rubles for 3 years, you have to pay the bank " + monthlyPayment3 + " rubles a month");
     }
